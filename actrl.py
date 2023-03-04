75ea4dd9bb265c4127385504df24b93d31a7b665
Ryan Mounce <ryan@mounce.com.au>
2023-03-04 23:15:41 +1030

    big cleanup hope it works

--- actrl.py
+++ actrl.py
@@ -16,9 +16,6 @@ global_temp_deriv_window = 5
 # to predict 10 minutes into the future
 global_temp_deriv_factor = 10.0
 
-# report 0.25 degree offset to aircon as 1 degree, 4x amplification
-global_compress_factor = 0.25
-
 # per second
 global_ki = 0.00025
 # a 0.1 deg error will accumulate 0.1 in ~60 minutes
@@ -75,10 +72,6 @@ target_ramp_proportional = 0.2 * interval
 # linear below 0.1C
 target_ramp_linear_threshold = 0.1
 target_ramp_linear_increment = target_ramp_proportional * target_ramp_linear_threshold
-
-
-def clamp(min_val, input_val, max_val):
-    return min(max(min_val, input_val), max_val)
 
 
 class MyWMA:
@@ -195,79 +188,28 @@ class MyPID:
         )
 
 
-class MySimplerIntegral:
-    def __init__(self, ki, clamp_low, clamp_high):
-        self.ki = ki
-        self.clamp_low = clamp_low
-        self.clamp_high = clamp_high
-        self.clear()
-
-    def clear(self):
-        self.integral = 0.0
-
-    def set(self, error):
-        self.integral += error * self.ki
-        if self.integral < self.clamp_low:
-            self.integral = self.clamp_low
-        if self.integral > self.clamp_high:
-            self.integral = self.clamp_high
-
-    def get(self):
-        return self.integral
-
-
 class DeadbandIntegrator:
-    def __init__(self, ki, step_up_intervals, step_down_intervals):
+    def __init__(self, ki):
         self.ki = ki
-        self.step_up_intervals = step_up_intervals
-        self.step_down_intervals = step_down_intervals
         self.clear()
 
     def clear(self):
         self.integral = 0.0
-        self.ramp_count = 0
-
-    def clear_set(self, integral):
-        self.integral = integral
-        self.ramp_count = 0
 
     def set(self, error):
         self.integral += error * self.ki
-        print("input " + str(error) + " integral " + str(self.integral))
 
-        if abs(self.ramp_count) > 0 and (
-            (
-                self.ramp_count > self.step_up_intervals
-                or self.ramp_count < -self.step_down_intervals
-            )
-            or abs(self.integral - math.copysign(1.0, self.ramp_count)) > 2.0
-        ):
-            print("over thresh, resetting")
-            # print("step_intervals " + str(self.step_intervals))
-            print(
-                "other thingy "
-                + str(abs(self.integral - math.copysign(1.0, self.ramp_count)))
-            )
-            self.ramp_count = 0
+        rval = 0
 
-        if self.ramp_count == 0 and abs(self.integral) > 1.0:
-            print("over thresh, starting")
-            self.ramp_count = math.copysign(1, self.integral)
-            self.integral -= math.copysign(2.0, self.ramp_count)
-
-        print("ramp_count " + str(self.ramp_count))
-
-        if self.integral < -1:
-            self.integral = -1
         if self.integral > 1:
-            self.integral = 1
+            self.integral = min(1, self.integral - 2)
+            rval = 1
+        elif self.integral < -1:
+            self.integral = max(-1, self.integral + 2)
+            rval = -1
 
-        if abs(self.ramp_count) > 0:
-            print("aircon is ramping")
-            self.ramp_count += math.copysign(1, self.ramp_count)
-            return math.copysign(1, self.ramp_count)
-        else:
-            return 0
+        print(f"input: {error}, integral: {self.integral}, rval: {rval}")
+        return rval
 
     def get(self):
         return self.integral
@@ -284,12 +226,6 @@ class Actrl(hass.Hass):
             window=int(global_temp_deriv_window / interval),
             factor=int(global_temp_deriv_factor / interval),
         )
-        self.temp_integral = MySimplerIntegral(
-            ki=(global_ki * 60.0 * interval),
-            clamp_low=0,
-            clamp_high=global_compress_factor,
-        )
-        self.ramping_down = True
         self.prev_unsigned_compressed_error = 0
         self.min_power_counter = 0
         self.off_fan_running_counter = 0
@@ -307,8 +243,6 @@ class Actrl(hass.Hass):
 
         self.deadband_integrator = DeadbandIntegrator(
             ki=(global_deadband_ki * 60.0 * interval),
-            step_up_intervals=step_up_intervals,
-            step_down_intervals=step_down_intervals,
         )
 
         for room in rooms:
@@ -409,16 +343,12 @@ class Actrl(hass.Hass):
             self.get_entity("input_number.aircon_avg_deriv").set_state(
                 state=float("nan")
             )
-            self.get_entity("input_number.aircon_integrated_error").set_state(
-                state=float("nan")
-            )
             self.get_entity("input_number.aircon_meta_integral").set_state(
                 state=float("nan")
             )
             for room, pid in self.pids.items():
                 pid.clear()
             self.temp_deriv.clear()
-            self.temp_integral.clear()
             self.compressor_totally_off = True
             self.on_counter = 0
             self.deadband_integrator.clear()
@@ -523,34 +453,22 @@ class Actrl(hass.Hass):
         self.temp_deriv.set(weighted_error, weighted_target)
         avg_deriv = self.temp_deriv.get()
 
-        self.temp_integral.set(weighted_error)
-
         self.get_entity("input_number.aircon_weighted_error").set_state(
             state=weighted_error
         )
         self.get_entity("input_number.aircon_avg_deriv").set_state(state=avg_deriv)
-        self.get_entity("input_number.aircon_integrated_error").set_state(
-            state=self.temp_integral.get()
-        )
 
         self.log(
             "compressor_totally_off: "
             + str(self.compressor_totally_off)
             + ", on_counter: "
             + str(self.on_counter)
-            + ", weighted_error pre-integral: "
+            + ", weighted_error: "
             + str(weighted_error)
             + ", weighted_target: "
             + str(weighted_target)
         )
-        self.log(
-            "avg_deriv: "
-            + str(avg_deriv)
-            + ", temp_integral: "
-            + str(self.temp_integral.get())
-        )
-
-        weighted_error += self.temp_integral.get()
+        self.log("avg_deriv: " + str(avg_deriv))
 
         unsigned_compressed_error = self.compress(
             weighted_error * heat_cool_sign, avg_deriv * heat_cool_sign
@@ -561,12 +479,7 @@ class Actrl(hass.Hass):
             compressed_error = heat_cool_sign * min(unsigned_compressed_error, 0)
         else:
             compressed_error = heat_cool_sign * unsigned_compressed_error
-        self.log(
-            "weighted_error post-integral: "
-            + str(weighted_error)
-            + ", compressed_error: "
-            + str(compressed_error)
-        )
+        self.log("compressed_error: " + str(compressed_error))
         self.get_entity("input_number.fake_temperature").set_state(
             state=(main_setpoint + compressed_error)
         )
@@ -667,137 +580,127 @@ class Actrl(hass.Hass):
                 "climate/set_hvac_mode", entity_id="climate.aircon", hvac_mode=mode
             )
 
-    def actually_compress(self, error):
-        # tell the aircon that temp variations are worse than reality
-        # static 0.5 degree offset
-        return 0.5 + error / global_compress_factor
-
     def compress(self, error, deriv):
-        on_threshold = 1
-        off_threshold = -2
+        # when the error is greater than 2.0C
+        # let the Midea controller do its thing
+        faithful_threshold = 2.0
+        desired_on_threshold = 0.25
+        min_power_threshold = -0.25
+        desired_off_threshold = -0.5
 
-        # higher threshold in cool mode while already running
-        if self.get_state("climate.aircon") == "cool":
-            initial_on_threshold = 2
+        ac_celsius = True
+
+        # Midea constants depending on temp units
+        if ac_celsius:
+            ac_unit_from_celsius = 1.0
+            ac_on_threshold = 2
+            ac_stable_threshold = 1
+            ac_off_threshold = -2
         else:
-            initial_on_threshold = 1
+            # Fahrenheit
+            ac_unit_from_celsius = 1.8
+            # https://github.com/esphome/feature-requests/issues/1627#issuecomment-1365639966
+            ac_on_threshold = 3
+            ac_stable_threshold = 2
+            ac_off_threshold = -3
 
-        rval = self.actually_compress(error)
-
-        # conditions in which to consider the derivative
-        # - aircon is currently running
-        # - the current temp (ignoring RoC) has not yet reached off threshold
-        # goal of the derivative is to proactively reduce/increase compressor power, but not to influence on/off state
-        if not self.compressor_totally_off and (round(rval) > off_threshold):
-            rval = max((off_threshold + 1), self.actually_compress(error + deriv))
-
-        rval = clamp(-15, rval, 15)
-        unrounded_rval = rval
-        rval = round(rval)
-
-        if rval <= off_threshold:
+        if error <= desired_off_threshold:
             self.compressor_totally_off = True
-            # sometimes -2 isn't the true off_threshold?!
-            # shut things down more decisively
-            rval = min(rval, -3)
 
         if self.compressor_totally_off:
             self.on_counter = 0
             self.min_power_counter = 0
 
-            if rval < on_threshold:
-                return rval
+            if error < desired_on_threshold:
+                # sometimes -2 isn't the true off_threshold?!
+                # shut things down more decisively
+                return ac_off_threshold - 1
             else:
                 self.compressor_totally_off = False
                 self.temp_deriv.clear()
                 self.deadband_integrator.clear()
-                return initial_on_threshold
+                return ac_on_threshold
 
-        if self.on_counter < soft_delay and rval >= 0:
+        # conditions in which to consider the derivative
+        # - aircon is currently running
+        # - the current temp (ignoring RoC) has not yet reached off threshold
+        # goal of the derivative is to proactively reduce/increase compressor power, but not to influence on/off state
+        error = error + deriv
+
+        if self.on_counter < soft_delay and error > 0:
             print("soft start, on_counter: " + str(self.on_counter))
-            self.ramping_down = True
-            return 0
+            return ac_stable_threshold - 1
 
-        if self.on_counter < (soft_delay + soft_ramp) and rval > 2:
-            ramp_progress = (self.on_counter - soft_delay) / soft_ramp
-            print(
-                "ramping "
-                + str(ramp_progress)
-                + ", on_counter: "
-                + str(self.on_counter)
-            )
-            return round((ramp_progress * unrounded_rval) + ((1.0 - ramp_progress) * 1))
-
-        if 0 <= rval and rval <= 2:
-            unrounded_rval = (
-                self.deadband_integrator.set(
-                    (unrounded_rval - 1.0) * global_compress_factor
+        if error > faithful_threshold:
+            if self.on_counter < (soft_delay + soft_ramp):
+                ramp_progress = (self.on_counter - soft_delay) / soft_ramp
+                print(
+                    "ramping "
+                    + str(ramp_progress)
+                    + ", on_counter: "
+                    + str(self.on_counter)
                 )
-                + 1.0
+            else:
+                ramp_progress = 1
+
+            return round(
+                ac_stable_threshold
+                + ramp_progress
+                * (2 + ac_unit_from_celsius * (error - faithful_threshold))
             )
-            rval = round(unrounded_rval)
-        else:
-            self.deadband_integrator.clear()
 
-        # self.outer_ramp_count = 0
-        # self.outer_ramp_rval = 1
-        if (
-            (self.outer_ramp_count > 0 and rval < 1)
-            or (self.outer_ramp_count < 0 and rval > 1)
-            or (self.outer_ramp_count > step_up_intervals)
-            or (self.outer_ramp_count < -step_down_intervals)
-        ):
-            self.outer_ramp_count = 0
-
-        # restart if the temp goes even further in the direction we're holding
-        if rval > 1 and rval > self.outer_ramp_rval:
-            self.outer_ramp_rval = rval
-            self.outer_ramp_count = 1
-        elif rval < 1 and rval < self.outer_ramp_rval:
-            self.outer_ramp_rval = rval
-            self.outer_ramp_count = -1
-
-        if self.outer_ramp_count > 0:
-            self.outer_ramp_count += 1
-            rval = self.outer_ramp_rval
-        elif self.outer_ramp_count < 0:
-            self.outer_ramp_count -= 1
-            rval = self.outer_ramp_rval
-        else:
-            self.outer_ramp_rval = rval
-
-        # aircon seems to react to edges
-        # so give 'em as many as possible
-        if rval > 1:
-            rval = min(self.prev_unsigned_compressed_error + 1, rval)
-        elif rval < 1:
-            rval = max(rval, self.prev_unsigned_compressed_error - 1)
-
-        if rval <= 0:
-            self.ramping_down = True
-        # if rval goes below 0 then we wanted min power
-        # WRONG: no need to arrest the fall in that case
-        # the 'blip' is needed in order to maintain min power...
-        # elif rval < 0:
-        #    self.ramping_down = False
-
-        if rval == -1:
+        if error < min_power_threshold:
             self.min_power_counter += 1
             if self.min_power_counter <= min_power_time:
-                return -1
+                # aircon seems to react to edges
+                # so provide as many as possible to quickly reduce power?
+                # may be extra helpful in fahrenheit mode?
+                return max(
+                    ac_off_threshold + 1, self.prev_unsigned_compressed_error - 1
+                )
             else:
-                return 0
+                return ac_off_threshold + 2
         else:
             self.min_power_counter = 0
+
+        if self.prev_unsigned_compressed_error > ac_stable_threshold + 1:
+            step = 1
+            self.deadband_integrator.clear()
+        elif self.prev_unsigned_compressed_error < ac_stable_threshold - 1:
+            step = -1
+            self.deadband_integrator.clear()
+        else:
+            step = self.deadband_integrator.set(error)
+
+        if abs(self.outer_ramp_count) > 0 and (
+            self.outer_ramp_count > step_up_intervals
+            or self.outer_ramp_count < -step_down_intervals
+        ):
+            print("count is up, step is over")
+            self.outer_ramp_count = 0
+
+        if step != 0:
+            print("(re)starting due to step")
+            self.outer_ramp_count = step
+
+        print("ramp_count " + str(self.outer_ramp_count))
+
+        if abs(self.ramp_count) > 0:
+            print(f"aircon is ramping, count: {self.outer_ramp_count}")
+            self.ramp_count += math.copysign(1, self.outer_ramp_count)
+            rval = math.copysign(1, self.outer_ramp_count)
+        else:
+            rval = 0
 
         # behaviour only observed in cooling mode
         # at the setpoint ac will start ramping back power
         # and will continue ramping back at setpoint+1
         # a brief jump up to setpoint+2 is needed to arrest the fall and stabilise output power
         # in heating mode there is no gradual rampdown at all, it drops straight to min power
-
-        if self.ramping_down and rval > 0:
-            self.ramping_down = False
-            return max(rval, initial_on_threshold)
+        if (
+            self.prev_unsigned_compressed_error < ac_stable_threshold
+            and rval >= ac_stable_threshold
+        ):
+            return max(rval, ac_stable_threshold + 1)
 
         return rval
