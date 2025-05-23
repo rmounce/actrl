89401146d5741ea0a73ad0bea4aa13fa144d4fea
Ryan Mounce <ryan@mounce.com.au>
2025-05-23 13:07:24 +0930

    embiggen incremental control window on overshoot

--- actrl.py
+++ actrl.py
@@ -125,7 +125,7 @@ desired_on_threshold = 0.0
 # After reducing to minimum power and holding for a while, turn off at a tighter threshold
 eventual_off_threshold = -0.5
 # Give up on incremental control and cut to minimum power to avoid overshooting
-min_power_threshold = -0.75
+min_power_threshold = -1.25
 # Worst case, turn off if we have overshot massively.
 immediate_off_threshold = -1.5
 
